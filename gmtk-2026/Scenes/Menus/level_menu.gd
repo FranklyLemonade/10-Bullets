7f9de5b2010660105f40ea4dcd8a5ecfd98068c2
Iamsoawesom_e <151198690+FranklyLemonade@users.noreply.github.com>
2026-07-24 20:17:29 -0400

    Refactor level structure and add level menu styling
    - Removed BaseLevel.tscn template and merged content into Level0.tscn
    - Added level_button.png texture asset for level menu buttons
    - Updated LevelMenu buttons to use new texture with proper sizing and positioning
    - Fixed level_menu.gd references and updated scene path handling
    - Changed start_menu to navigate to LevelMenu instead of BaseLevel

--- gmtk-2026/Scenes/Menus/level_menu.gd
+++ gmtk-2026/Scenes/Menus/level_menu.gd
@@ -1,6 +1,6 @@
 extends Control
 
-@onready var level_selector = %HBoxContainer
+@onready var level_selector = $TextureRect/HBoxContainer
 var buttons = []
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,8 +9,8 @@ func _ready() -> void:
 			buttons.append(child)
 			child.pressed.connect(_on_button_pressed.bind(child))
 
-func _on_button_pressed(button_node: Button) -> void:
-	get_tree().change_scene_to_file("res://Scenes/Levels/Level" + str(buttons.find(button_node)))
+func _on_button_pressed(button_node: TextureButton) -> void:
+	get_tree().change_scene_to_file("res://Scenes/Levels/Level" + str(buttons.find(button_node)) + ".tscn")
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
